40ae22ae71428432c12f2c13ac9f396681894b47
이동규 <ehdrb3663@c3coding.com>
2023-05-08 12:13:50 +0900

    업데이트

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,15 @@
 import '@/styles/globals.css'
 import wrapper from '@/lib/store/configureStore'
-import PropTypes from "prop-types";
-import { SessionProvider } from "next-auth/react"
 import { Provider } from 'react-redux';
 
-function App({ 
-  Component, 
-  // pageProps: { session, ...pageProps } 
-  ...rest
-}) {
+const App = ({ Component, ...rest}) => {
   const { store, props } = wrapper.useWrappedStore(rest);
+  
   return (
-    // <SessionProvider session={session}>
-    //   <Component {...pageProps} />
-    // </SessionProvider>
     <Provider store={store}>
       <Component {...props.pageProps}/>
     </Provider>
   )
 }
-
-// App.propTypes = {
-//   Component: PropTypes.elementType.isRequired,
-// };
-
-// export default wrapper.withRedux(App);
 
 export default App
